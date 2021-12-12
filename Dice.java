affaa6bb48099e5466f7b7c847387c8b8bbaf738
Anirudh Saladi <anirudhsaladi7@gmail.com>
2021-12-12 14:12:41 +0530

    Updated Dice.java
    Added three sixes in a row logic.

--- Dice.java
+++ Dice.java
@@ -18,3 +18,50 @@ public class Dice {
 
     }
 }
+
+class SingleLogicDice extends Dice {
+    private int noOfDice = 1;
+
+    SingleLogicDice() {
+        super(1);
+    }
+
+    // public int rollDice() {
+    // int ran = ((int) (Math.random() * (6 - 1))) + 1;
+    // if (ran == 6) {
+    // ran = 6 + rollDice();
+    // if (ran % 18 == 0) {
+    // ran = 0;
+    // return ran;
+    // }
+    // }
+    // return ran;
+    // }
+
+    // 3 6's LOGIC
+
+    public int rollDice() {
+        int ran = ((int) (Math.random() * (6 - 1))) + 1;
+
+        if (ran == 6) {
+            while (true) {
+                ran = 6 + ((int) (Math.random() * (6 - 1))) + 1;
+                if (ran == 12) {
+                    ran = 12 + ((int) (Math.random() * (6 - 1))) + 1;
+                    if (ran == 18) {
+                        System.out.println("3 6's in row. Hard Luck!");
+                        ran = 0;
+                        break;
+                    } else {
+                        break;
+                    }
+
+                } else {
+                    break;
+                }
+            }
+        }
+        return ran;
+    }
+
+}
